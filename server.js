522771808ce729a5442696622cc776cc96f7e62e
daanzho1 <barrywilliams9000@gmail.comgit config --global user.name Your>
2022-09-27 19:46:40 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 
 // Configuration
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
+
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
 
 
 // ROUTES
@@ -20,9 +29,17 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
+
+
 // Listen
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 })
+
+
 
 console.log(PORT)
